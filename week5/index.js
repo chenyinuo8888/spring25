8d81323e874865ef43b785433fb0a4c3bfb767b5
chenyinuo8888 <chenyinuo8888@gmail.com>
2025-03-21 12:18:23 -0700

    week7

--- week5/index.js
+++ week5/index.js
@@ -4,23 +4,31 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const mongoose = require("mongoose");
 
-app.get('/', (req, res) => {
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+  content: { type: String }
+})
+
+const messageModel = mongoose.model("Message", messageSchema)
+
+app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
-
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    const message = new messageModel();
+    message.content = msg;
+    message.save().then(m => {
+      io.emit('chat message', msg);
+    })
   });
 });
-io.on('connection', (socket) => {
-  socket.broadcast.emit('hi');
+
+server.listen(3000, async function(){
+  await mongoose.connect("mongodb+srv://sam:helloworld@cluster0.ytnrk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  console.log('listening on *:3000');
 });
